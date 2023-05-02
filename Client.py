1b1296731814943c89a3e948c9f234b89628d38f
Vidarpettersen <vidar.pettersen@smbpartner.no>
2023-05-02 14:11:26 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -19,20 +19,17 @@ class Client():
         while self.active:
             if blacklist.isBlacklisted(self.address):
                 return
-            try:
-                self.clientsocket.settimeout(config.SOCKET_TIMEOUT)
-                data = self.clientsocket.recv(1024).hex()
-                if self.imei == "":
 
-                    self.imei = codecs.decode(''.join(data),'hex').decode('ascii')
-                    print(self.imei)
-                    continue
-                decoded = Decode(data)
-                for json in decoded.toApi():
-                    self.sendToApi(self, json)
-            except:
-                print("Error")
-                return     
+            self.clientsocket.settimeout(config.SOCKET_TIMEOUT)
+            data = self.clientsocket.recv(1024).hex()
+            if self.imei == "":
+                self.imei = codecs.decode(''.join(data),'hex').decode('ascii')
+                print(self.imei)
+                continue
+            decoded = Decode(data)
+            for json in decoded.toApi():
+                self.sendToApi(self, json)
+
         
     def sendToApi(self, data):
         json={"token": self.imei, "data": data}
