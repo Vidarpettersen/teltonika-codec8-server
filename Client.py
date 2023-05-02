ad565a2a8a28105362bc6d27f4d59285c6249d3b
Vidarpettersen <vidar.pettersen@smbpartner.no>
2023-05-02 14:25:12 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -27,13 +27,13 @@ class Client():
                     continue
                 decoded = Decode(data)
                 for json in decoded.toApi():
-                    self.sendToApi(json.decode('utf-8'))
+                    self.sendToApi(json)
             except:
                 return
 
         
     def sendToApi(self, data):
-        json={"token": self.imei, "data": data}
+        json={"token": self.imei, "data": "["+data+"]"}
         try:
             r = requests.post(config.API_ADDRESS, json=json)
             if r.status_code:
